fcf37bea61d9060f88faba14a7de823f759f23ea
Lucas De Andrade Forte <lucasdeandradeforte@gmail.com>
2022-10-17 23:59:53 -0300

    :recycle: refactor: Passando function components - typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,25 @@ import {
   useRef,
   useState,
   useCallback,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>, HTMLInputElement
+  > {
+  name: string
+  icon?: any
+}
+
+export function Input(
+  { name, icon: Icon, ...rest }: InputProps
+) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,7 +36,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!(inputRef.current as any)?.value);
   }, []);
 
   useEffect(() => {
@@ -49,5 +61,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
